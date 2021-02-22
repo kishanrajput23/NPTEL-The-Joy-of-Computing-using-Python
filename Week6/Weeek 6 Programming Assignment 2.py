975b344f2a02bf68a8c0673cd599dcadca149e1b
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2021-02-22 21:12:18 +0530

    assignment 2

---
+++ Week6/Weeek 6 Programming Assignment 2.py
@@ -0,0 +1,24 @@
+str_1 = input()
+
+str_2 = input()
+
+sorted_str1 = []
+
+sorted_str2 = []
+
+k = sorted(str_1.upper())
+
+l = sorted(str_2.upper())
+
+for i in k:
+  if i.isalnum():
+    sorted_str1.append(i)
+    
+for j in l:
+  if j.isalnum():
+    sorted_str2.append(j)
+    
+if sorted_str1 == sorted_str2:
+  print("Yes")
+else:
+  print("No")
